df0e65ccdf3a156f7e53c1be68b1556aa8497675
Lex Li <support@lextm.com>
2016-07-12 20:59:04 +0800

    Disabled obfuscation.

--- obfuscate.bat
+++ obfuscate.bat
@@ -1,5 +1,5 @@
 mkdir bin
 cd bin
-call ..\packages\Obfuscar.2.2.0\tools\Obfuscar.Console.exe jexus.xml
+echo call ..\packages\Obfuscar.2.2.0\tools\Obfuscar.Console.exe jexus.xml
 @IF %ERRORLEVEL% NEQ 0 PAUSE
 cd ..
