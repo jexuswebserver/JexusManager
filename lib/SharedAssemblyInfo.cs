6aaf5815dc21303c92ac4ce7c27193b2034515da
Lex Li <support@lextm.com>
2017-07-01 23:32:21 -0400

    Upgraded packages and added a test script.

--- lib/SharedAssemblyInfo.cs
+++ lib/SharedAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("11.0.000614.03")]
+[assembly: AssemblyVersion("11.0.000701.02")]
 #if (!CF)
-[assembly: AssemblyFileVersion("11.0.000614.03")]
+[assembly: AssemblyFileVersion("11.0.000701.02")]
 #endif
