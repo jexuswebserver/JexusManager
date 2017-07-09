e47afc015590b5c1d60af8799de74526b5d9060a
Lex Li <support@lextm.com>
2017-07-09 15:40:02 -0400

    Added Rollbar crash report.

--- JexusManager/Program.cs
+++ JexusManager/Program.cs
@@ -2,7 +2,11 @@
 // 
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Microsoft.Web.Administration;
+using Microsoft.Win32;
+using RollbarDotNet;
 
 namespace JexusManager
 {
@@ -15,15 +19,72 @@ namespace JexusManager
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        private static void Main()
+        private static void Main(string[] args)
         {
+            var suppress = false;
+            foreach (var arg in args)
+            {
+                if (arg == "-s")
+                {
+                    suppress = true;
+                }
+            }
+
+            if (!suppress)
+            {
+                var key = Registry.CurrentUser.CreateSubKey(@"Software\LeXtudio\JexusManager");
+                if (key != null)
+                {
+                    var asked = (string)key.GetValue("RollbarAsked", "false");
+                    if (!string.Equals(asked, "true", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (MessageBox.Show(
+                                "This application uses Rollbar to collect crash information. Click Yes to continue and allow Rollbar to work. Click No to exit.",
+                                "Information",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Information) != DialogResult.Yes)
+                        {
+                            return;
+                        }
+                    
+                        key.SetValue("RollbarAsked", "true");
+                    }
+                }
+
+                SetupRollbar();
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
+            
             // TODO: set encryption support
             // ProtectedConfigurationProvider.Provider = new WorkingEncryptionServiceProvider();
 
             Application.Run(new MainForm());
+        }
+
+        private static void SetupRollbar()
+        {
+            Rollbar.Init(new RollbarConfig
+                            {
+                                AccessToken = "5525758f15504199b7125d35d2058cfe",
+                                Environment = "production"
+                            });
+            
+            Application.ThreadException += (sender, args) =>
+            {
+                Rollbar.Report(args.Exception);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Rollbar.Report(args.ExceptionObject as System.Exception);
+            };
+
+            TaskScheduler.UnobservedTaskException += (sender, args) =>
+            {
+                Rollbar.Report(args.Exception);
+            };
         }
     }
 }
