ac950697e7a12128fa1c789ca1d6bd2153e65354
Lex Li <support@lextm.com>
2018-07-04 20:06:43 -0400

    Updated .NET Framework version detection.

--- JexusManager/Program.cs
+++ JexusManager/Program.cs
@@ -156,25 +156,24 @@ namespace JexusManager
         // Checking the version using >= will enable forward compatibility.
         private static string CheckFor45PlusVersion(int releaseKey)
         {
+            if (releaseKey >= 461808)
+                return "4.7.2 or later";
+            if (releaseKey >= 461308)
+                return "4.7.1";
             if (releaseKey >= 460798)
-                return "4.7 or later";
+                return "4.7";
             if (releaseKey >= 394802)
                 return "4.6.2";
-            if (releaseKey >= 394254) {
+            if (releaseKey >= 394254)
                 return "4.6.1";
-            }
-            if (releaseKey >= 393295) {
+            if (releaseKey >= 393295)
                 return "4.6";
-            }
-            if ((releaseKey >= 379893)) {
+            if (releaseKey >= 379893)
                 return "4.5.2";
-            }
-            if ((releaseKey >= 378675)) {
+            if (releaseKey >= 378675)
                 return "4.5.1";
-            }
-            if ((releaseKey >= 378389)) {
+            if (releaseKey >= 378389)
                 return "4.5";
-            }
             // This code should never execute. A non-null release key should mean
             // that 4.5 or later is installed.
             return "No 4.5 or later version detected";
