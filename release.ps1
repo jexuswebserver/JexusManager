fcf547362bce479ad22362822b20e6891d591913
Lex Li <support@lextm.com>
2018-09-18 17:14:20 -0400

    Fixed the build script.

--- release.ps1
+++ release.ps1
@@ -1,11 +1,24 @@
-Install-Module VSSetup -Scope CurrentUser -Force
-$instance = Get-VSSetupInstance -All | Select-VSSetupInstance -Require 'Microsoft.Component.MSBuild' -Latest
-$installDir = $instance.installationPath
-$msBuild = $installDir + '\MSBuild\15.0\Bin\MSBuild.exe'
-if (![System.IO.File]::Exists($msBuild))
+$msBuild = "msbuild"
+& $msBuild /version
+if ($LastExitCode -ne 0)
 {
-    Write-Host "MSBuild doesn't exist. Exit."
-    exit 1
+    Write-Host "MSBuild doesn't exist. Use VSSetup instead."
+
+    Install-Module VSSetup -Scope CurrentUser -Force
+    $instance = Get-VSSetupInstance -All | Select-VSSetupInstance -Require 'Microsoft.Component.MSBuild' -Latest
+    $installDir = $instance.installationPath
+    $msBuild = $installDir + '\MSBuild\15.0\Bin\MSBuild.exe'
+    if (![System.IO.File]::Exists($msBuild))
+    {
+        Write-Host "MSBuild doesn't exist. Exit."
+        exit 1
+    }
+
+    Write-Host "Likely on Windows."
+}
+else
+{
+    Write-Host "Likely on Linux/macOS."
 }
 
 Write-Host "MSBuild found. Compile the projects."
