1f469bd849178536e05488d95d86a9cdcaa53867
Lex Li <support@lextm.com>
2019-01-03 22:19:06 -0500

    Fixed FRT UI bug and also fixed FRT folder query.

--- Microsoft.Web.Administration/SiteTraceFailedRequestsLogging.cs
+++ Microsoft.Web.Administration/SiteTraceFailedRequestsLogging.cs
@@ -8,27 +8,73 @@ namespace Microsoft.Web.Administration
 {
     public sealed class SiteTraceFailedRequestsLogging : ConfigurationElement
     {
+        private readonly SiteDefaultProcessor<SiteTraceFailedRequestsLogging> _processor;
+
         internal SiteTraceFailedRequestsLogging(ConfigurationElement element, ConfigurationElement parent)
             : base(element, "traceFailedRequestsLogging", null, parent, null, null)
         {
+            var server = (parent as Site)?.Server;
+            _processor = new SiteDefaultProcessor<SiteTraceFailedRequestsLogging>(server?.SiteDefaults.TraceFailedRequestsLogging);
         }
 
         public string Directory
         {
-            get { return (string)this["directory"]; }
-            set { this["directory"] = value; }
+            get
+            {
+                return _processor.Get("directory",
+                    () => _processor.SiteDefault.Directory,
+                    attribute => (string)attribute.Value,
+                    this);
+            }
+
+            set
+            {
+                _processor.Set("directory",
+                    value,
+                    () => _processor.SiteDefault.Directory,
+                    attribute => attribute.Value = value,
+                    this);
+            }
         }
 
         public bool Enabled
         {
-            get { return (bool)this["enabled"]; }
-            set { this["enabled"] = value; }
+            get
+            {
+                return _processor.Get("enabled",
+                    () => _processor.SiteDefault.Enabled,
+                    attribute => (bool)attribute.Value,
+                    this);
+            }
+
+            set
+            {
+                _processor.Set("enabled",
+                    value,
+                    () => _processor.SiteDefault.Enabled,
+                    attribute => attribute.Value = value,
+                    this);
+            }
         }
 
         public long MaxLogFiles
         {
-            get { return Convert.ToInt64(this["maxLogFiles"]); }
-            set { this["maxLogFiles"] = Convert.ToUInt32(value); }
+            get
+            {
+                return _processor.Get("maxLogFiles",
+                    () => _processor.SiteDefault.MaxLogFiles,
+                    attribute => Convert.ToInt64(attribute.Value),
+                    this);
+            }
+
+            set
+            {
+                _processor.Set("maxLogFiles",
+                    value,
+                    () => _processor.SiteDefault.MaxLogFiles,
+                    attribute => attribute.Value = Convert.ToUInt32(value),
+                    this);
+            }
         }
     }
 }
