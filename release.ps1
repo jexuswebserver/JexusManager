16686ad3f7bc8bba7dbc013b0f57aada7e1522f8
Lex Li <support@lextm.com>
2019-06-01 23:18:50 -0400

    Fixed VS2017 support.

--- release.ps1
+++ release.ps1
@@ -14,11 +14,22 @@ catch
     $msBuild = $installDir + '\MSBuild\Current\Bin\MSBuild.exe'
     if (![System.IO.File]::Exists($msBuild))
     {
-        Write-Host "MSBuild doesn't exist. Exit."
-        exit 1
+        Write-Host "MSBuild 16 doesn't exist."
+        $msBuild = $installDir + '\MSBuild\15.0\Bin\MSBuild.exe'
+        if (![System.IO.File]::Exists($msBuild))
+        {
+            Write-Host "MSBuild 15 doesn't exist. Exit."
+            exit 1
+        }
+        else
+        {
+            Write-Host "Likely on Windows with VS2017."
+        }
     }
-
-    Write-Host "Likely on Windows with VS2019."
+    else
+    {
+        Write-Host "Likely on Windows with VS2019."
+    }
 }
 
 Write-Host "MSBuild found. Compile the projects."
