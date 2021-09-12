d558704795d70bd0ede31cebbdac3274d7523010
Lex Li <support@lextm.com>
2021-09-11 17:03:00 -0700

    Improved build scripts.

--- dist.all.bat
+++ dist.all.bat
@@ -1,4 +1,4 @@
-powershell -file download.dotnet.releases.ps1
+powershell -ExecutionPolicy Bypass -file download.dotnet.releases.ps1
 call dist.deploy.bat
-powershell -file sha1.ps1
+powershell -ExecutionPolicy Bypass -file sha1.ps1
 pause
--- dist.deploy.bat
+++ dist.deploy.bat
@@ -1,13 +1,13 @@
-powershell -file release.ps1
+powershell -ExecutionPolicy Bypass -file release.ps1
 IF %ERRORLEVEL% NEQ 0 goto failed
 
-powershell -file sign.ps1
+powershell -ExecutionPolicy Bypass -file sign.ps1
 IF %ERRORLEVEL% NEQ 0 goto failed
 
 CALL build.installer.bat
 IF %ERRORLEVEL% NEQ 0 goto failed
 
-powershell -file sign.installers.ps1
+powershell -ExecutionPolicy Bypass -file sign.installers.ps1
 IF %ERRORLEVEL% NEQ 0 goto failed
 
 echo succeeded.
