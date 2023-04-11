e3af05a92f45bc9487b77db164a47a1d515ddb94
Lex Li <support@lextm.com>
2023-04-11 03:11:43 -0400

    Fixed ARM64 installer issues.

--- build.installer.bat
+++ build.installer.bat
@@ -1,9 +1,9 @@
 del bin\Release\en-us\JexusManager.msi
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x86
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=arm64
+copy bin\Release\en-us\JexusManager.msi .\JexusManager_arm64.msi
+del bin\Release\en-us\JexusManager.msi
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x86
 copy bin\Release\en-us\JexusManager.msi .\JexusManager_x86.msi
 del bin\Release\en-us\JexusManager.msi
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x64
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x64
 copy bin\Release\en-us\JexusManager.msi .\JexusManager_x64.msi
-del bin\Release\en-us\JexusManager.msi
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=ARM64
-copy bin\Release\en-us\JexusManager.msi .\JexusManager_arm64.msi
