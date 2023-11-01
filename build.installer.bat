38acc279f9598402414bc7e9bd6fd8b7991990cc
Lex Li <support@lextm.com>
2023-11-01 03:20:48 -0400

    Removed x86 installer.

--- build.installer.bat
+++ build.installer.bat
@@ -2,8 +2,5 @@ del bin\Release\en-us\JexusManager.msi
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=arm64
 copy bin\Release\en-us\JexusManager.msi .\JexusManager_arm64.msi
 del bin\Release\en-us\JexusManager.msi
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x86
-copy bin\Release\en-us\JexusManager.msi .\JexusManager_x86.msi
-del bin\Release\en-us\JexusManager.msi
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Setup\JexusManager.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x64
 copy bin\Release\en-us\JexusManager.msi .\JexusManager_x64.msi
