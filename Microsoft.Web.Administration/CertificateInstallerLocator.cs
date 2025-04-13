500938404cec9772ee4004a331d2bfb7e3af13af
Lex Li <support@lextm.com>
2025-04-12 23:23:16 -0400

    Changed minimal Windows version to 10.

--- Microsoft.Web.Administration/CertificateInstallerLocator.cs
+++ Microsoft.Web.Administration/CertificateInstallerLocator.cs
@@ -23,13 +23,13 @@ namespace Microsoft.Web.Administration
                 return defaultPath;
             }
 
-            var debugBuild = Path.Combine(Environment.CurrentDirectory, $@"..\..\..\..\CertificateInstaller\bin\Debug\net9.0-windows\{executable}");
+            var debugBuild = Path.Combine(Environment.CurrentDirectory, $@"..\..\..\..\CertificateInstaller\bin\Debug\net9.0-windows10.0.17763.0\{executable}");
             if (File.Exists(debugBuild))
             {
                 return debugBuild;
             }
 
-            var releaseBuild = Path.Combine(Environment.CurrentDirectory, $@"..\..\..\..\CertificateInstaller\bin\Release\net9.0-windows\{executable}");
+            var releaseBuild = Path.Combine(Environment.CurrentDirectory, $@"..\..\..\..\CertificateInstaller\bin\Release\net9.0-windows10.0.17763.0\{executable}");
             if (File.Exists(releaseBuild))
             {
                 return releaseBuild;
